ebaeed7e3e30a615523a9fead3e269e04012cea5
JHonaker <honaker215@gmail.com>
2016-12-28 14:30:50 -0500

    Improved terating through entities for each system The system manager listens for changes to the components of entities and sends a message to the system to update its set of applicable entities. The systems no longer have to compute the valid entities every update cycle. The valid entities are only computed if the component added is in the RequiredComponents list and on add or remove from an entity.

--- fission.py
+++ fission.py
@@ -9,20 +9,52 @@ class Component(ABC):
 
 class System(ABC):
 
-    def __init__(self, entityManager, systemManager,
-                 messageDispatcher, requiredComponents):
-        self._entityManager = entityManager
-        self._systemManager = systemManager
-        self._messageDispatcher = messageDispatcher
+    def __init__(self, requiredComponents):
+        self._entityManager = None
+        self._systemManager = None
+        self._messageDispatcher = None
         self._requiredComponents = requiredComponents
+        self._entities = set()
+
+    def register(self, systemManager):
+        """
+        Register the system with its controlling system manager.
+
+        IMPORTANT: This must be called by the system manager before using the system.
+        """
+        self._systemManager = systemManager
+        self._entityManager = systemManager._entityManager
+        self._messageDispatcher = systemManager._messageDispatcher
+        self._postRegistrationSetup()
+
+    def _postRegistrationSetup(self):
+        pass
 
     @property
     def requiredComponents(self):
         return self._requiredComponents
 
+    @property
+    def entities(self):
+        return self._entities
+
+    def refreshEntity(self, entity):
+        inSystem = True
+        for componentType in self._requiredComponents:
+            inSystem = inSystem and self._entityManager.hasComponent(entity, componentType)
+
+        if inSystem:
+            self._entities.add(entity)
+        else:
+            self._entities.discard(entity)
+
+    def refreshAllEntities(self):
+        self._entities = self._entityManager.getEntitiesWithTypeList(self._requiredComponents)
+
     def components(self):
-        return(self._entityManager
-               .getAllEntitiesWithTypes(self.requiredComponents))
+        return ({componentType: self._entityManager.getComponent(entity, componentType)
+            for componentType in self._requiredComponents}
+            for entity in self._entities)
 
     @abstractmethod
     def update(self, delta):
@@ -73,10 +105,12 @@ class EntityManager:
             self._components[componentType] = {}
 
         self._components[componentType][entity] = component
+        self._messageDispatcher.send("componentAdded", entity, componentType)
 
     def removeComponentFrom(self, entity, componentType):
         try:
             del self._components[componentType][entity]
+            self._messageDispatcher("componentRemoved", entity, componentType)
             if self._components[componentType] == {}:
                 del self._components[componentType]
         except KeyError:
@@ -85,7 +119,6 @@ class EntityManager:
     def hasComponent(self, entity, componentType):
         if (componentType in self._components and
                 entity in self._components[componentType]):
-
             return True
         else:
             return False
@@ -109,7 +142,7 @@ class EntityManager:
                 components[componentType] = instances[entity]
         return(components)
 
-    def getAllEntitiesWithType(self, componentType):
+    def getAllEntityMapsWithType(self, componentType):
         """
         Returns a dictionary with the key-value pairs:
         key: Entity ID
@@ -117,7 +150,7 @@ class EntityManager:
         """
         return(self._components[componentType])
 
-    def getAllEntitiesWithTypes(self, types):
+    def getAllEntityMapsWithTypes(self, types):
         componentList = [self.getAllEntitiesWithType(t) for t in types]
         keys = self._entities
         for componentType in componentList:
@@ -129,23 +162,65 @@ class EntityManager:
 
         return(entities)
 
+    def getEntitiesWithType(self, componentType):
+        """
+        Returns a set of entity ids of entities with the component type.
+        """
+        try:
+            return set(self._components[componentType].keys())
+        except KeyError:
+            return set()
+
+    def getEntitiesWithTypeList(self, types):
+        entities = set(self._entities)
+        for componentType in types:
+            entities &= self.getEntitiesWithType(componentType)
+
+        return entities
+
+
 
 class SystemManager:
 
     def __init__(self, entityManager):
         self._systems = {}
         self._entityManager = entityManager
+        self._messageDispatcher = entityManager._messageDispatcher
+
+        self._messageDispatcher.subscribe("entityRemoved", self.handleEntityRemoval)
+        self._messageDispatcher.subscribe("componentAdded", self.handleComponentAddedOrRemoved)
+        self._messageDispatcher.subscribe("componentRemoved", self.handleComponentAddedOrRemoved)
+
 
     @property
     def systems(self):
         return(self._systems)
 
     def addSystem(self, system):
+        print("adding")
+        print(type(system))
         systemType = type(system)
+        system.register(self)
         self._systems[systemType] = system
 
+
     def removeSystem(self, system):
-        pass
+        try:
+            del self._systems[type(system)]
+        except:
+            pass
+
+    def handleEntityRemoval(self, entity):
+        for systemType in self._systems:
+            system = self._systems[systemType]
+            if entity in system.entities:
+                system.refreshEntity(entity)
+
+    def handleComponentAddedOrRemoved(self, entity, componentType):
+        for systemType in self._systems:
+            system = self._systems[systemType]
+            if componentType in system.requiredComponents:
+                system.refreshEntity(entity)
 
     def update(self, delta):
         for t, system in self._systems.items():
@@ -174,7 +249,7 @@ class MessageDispatcher:
 
     def send(self, messageType, *args, **kargs):
         if messageType not in self._messageTypes:
-            print("Nobody responds to " + str(messageType) + " messages")
+            pass
         else:
             for callback in self._messageTypes[messageType]:
                 callback(*args, **kargs)
